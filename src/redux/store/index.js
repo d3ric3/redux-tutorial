518f7bdde52b7b7ef9ab03da2dc1cd77f08a295c
Derice <d3ric3@gmail.com>
2020-11-02 00:13:28 +0800

    added comment at store/index.js

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,6 +2,19 @@ import { combineReducers, createStore } from "redux";
 
 import * as reducers from "../reducers";
 
+/*
+state name in REDUX is determine at combineReducers
+
+Example to have myCount state: 
+const allReducers = combineReducers({
+  myCount: counterReducer
+});
+
+const store = createStore(allReducers);
+
+const count = useSelector(state => state.myCount);
+*/
+
 const allReducers = combineReducers(reducers);
 
 const store = createStore(
